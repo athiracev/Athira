5381bcff6cdc9c2f4e3efa278cdcbb4fea12fc96
athiracev <94682674+athiracev@users.noreply.github.com>
2022-01-04 21:15:54 +0530

    Create linearsearch.c

---
+++ linearsearch.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+        int arr[50],search,i,n;
+        printf("Enter the number of elements in array\n");
+        scanf("%d",&n);
+        printf("Enter %d integers\n",n);
+        for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+        printf("Enter the number to search\n");
+        scanf("%d",&search);
+
+
+        for(i=0;i<n;i++)
+        {       
+                if(arr[i]==search)
+                {
+                        printf("%d is present at the location %d",search,i+1);
+                        break;
+                }
+        }
+        if(i==n)
+        printf("%d isn't present in the array\n",search);
+
+}
